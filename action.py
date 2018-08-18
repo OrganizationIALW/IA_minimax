19321f33a1f579f8b8b002b9289f38f078f22b8d
LMare <marescaux.louis@gmail.com>
2018-08-18 19:02:20 +0200

    ajout des extensions

---
+++ action.py
@@ -0,0 +1,17 @@
+//TODO: definir la forme d'un combo
+
+
+function getCombos(@partie) {
+	var combos = [];
+	//TODO:
+	return @combos;
+}
+
+function simulateCombo(@partie, @combo) {
+	//TODO: 
+}
+
+
+function executeCombo(@combo) {
+	// Execute le combo (dans la vrai partie LW)
+}
